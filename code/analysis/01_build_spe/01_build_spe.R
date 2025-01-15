34ca6536cf74ee85d018fbf273935fd17dedde2b
cindyfang70 <cindyfang70@gmail.com>
2025-01-15 15:10:24 -0500

    script for reading data into SPE objects

---
+++ code/analysis/01_build_spe/01_build_spe.R
@@ -0,0 +1,12 @@
+library(here)
+library(SpatialExperiment)
+
+
+########################################################
+# Read in the raw Xenium data into SPE objects and save.
+# Locations:
+# /dcs04/lieber/lcolladotor/rawDataTDSC_LIBD001/raw-data/2024-11-12_Xenium-pnn-SKCCC/
+#/dcs04/lieber/lcolladotor/rawDataTDSC_LIBD001/raw-data/2024-11-18_xenium-pnn-SKCCC/
+#/dcs04/lieber/lcolladotor/rawDataTDSC_LIBD001/raw-data/2024-12-06_xenium-pnn-SKCCC/
+#/dcs04/lieber/lcolladotor/rawDataTDSC_LIBD001/raw-data/2024-12-12_xenium-pnn-SKCCC/
+#######################################################
