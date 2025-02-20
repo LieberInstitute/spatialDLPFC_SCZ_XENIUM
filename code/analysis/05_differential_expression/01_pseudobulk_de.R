d6f1f788600e83d2493401da0063151f557dfc74
cindyfang70 <cindyfang70@gmail.com>
2025-02-20 17:05:12 -0500

    create psedobulk object

--- code/analysis/05_differential_expression/01_pseudobulk_de.R
+++ code/analysis/05_differential_expression/01_pseudobulk_de.R
@@ -15,24 +15,34 @@ suppressPackageStartupMessages({
 ####################################################################################################
 
 spe <- readRDS(here("processed-data", "01_build_spe", "raw_spe_N24.RDS"))
-spds <- readRDS(here("processed-data", "04_label_transfer", "label_transfer_N24_k50_smoothed_labels.csv"))
+spds <- read.csv(here("processed-data", "04_label_transfer", "label_transfer_N24_k50_smoothed_labels.csv"))
 
+rownames(spds) <- spds$X 
+spds$X <- NULL
+colnames(spds) <- c("predictions_smooth")
 spe <- spe[,colnames(spe) %in% rownames(spds)]
 colData(spe) <- merge(colData(spe), spds, by="row.names", all.x=TRUE)
+
+# Add some slide and date related metadata
+slide_id <- unlist(lapply(strsplit(spe$Sample, split="/"), "[", 9))
+slide_id <- unlist(lapply(strsplit(slide_id, split="__Br"), "[", 1))
+spe$slide_id <- slide_id
+
+run_date <- unlist(lapply(strsplit(spe$Sample, split="/"), "[", 7))
+spe$run_date <- run_date
+
 
 ## First, create pseudobulk data:
 # Reference: https://github.com/LieberInstitute/spatialDLPFC_SCZ/blob/main/code/analysis/pseudobulk_dx/create_pb_data.R
 
-spe_pseudo <-
-    registration_pseudobulk(
+spe_pseudo <- registration_pseudobulk(
       spe,
       var_registration = "predictions_smooth",
-      var_sample_id = "sample_id",
-      covars = c("Dx", "age", "sex", "lot_num", "slide_id"),
+      var_sample_id = "BrNum",
+      covars = c("Dx", "Age", "Sex", "slide_id", "slide_id", "run_date"),
       min_ncells = 10,
       pseudobulk_rds_file = here(
-        "processed-data", "rds", "layer_spd",
-        paste0("test_spe_pseudo_", .var, ".rds")
+        "processed-data", "05_differential_expression",
+        paste0("spe_pseudo_", "spaTransfer_k50_smoothed_predictions", ".rds")
       )
     )
-}
