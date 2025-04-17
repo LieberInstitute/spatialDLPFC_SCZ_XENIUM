6f8e70e1b341619babda6d8fa53436dd8ba53eeb
cindyfang70 <cindyfang70@gmail.com>
2025-04-17 11:37:17 -0400

    cell type DE code

---
+++ code/analysis/07_cell_type_de/01_create_cell_type_pseudobulk_spe.R
@@ -0,0 +1,86 @@
+suppressPackageStartupMessages({
+  library(here)
+  library(SpatialExperiment)
+  library(spatialLIBD)
+  library(scater)
+  library(tidyverse)
+  library(ggrepel)
+  library(sessioninfo)
+})
+
+####################################################################################################
+# Pseudobulk the SPE to the donor-domain level using the smoothed labels from label transfer.
+# Reference: https://github.com/LieberInstitute/spatialDLPFC_SCZ/blob/main/code/analysis/pseudobulk_dx/create_pb_data.R
+####################################################################################################
+
+spe <- readRDS(here("processed-data", "01_build_spe", "raw_spe_N24.RDS"))
+outlier_ids <- read.csv(here("processed-data", "02_xenium_qc", "outlier_ids.csv"))$x
+spe <- spe[, -which(colnames(spe) %in% outlier_ids)]
+rm (outlier_ids)
+spe <- spe[which(rowData(spe)$Type=="Gene Expression"), ]
+print(spe)
+
+#### Banksy parameters ####
+lambda <- 0.1
+res <- 0.7 # higher = more clusters
+cnm <- sprintf("clust_M%s_lam%s_k50_res%s", as.numeric(use_agf), lambda, res)
+
+
+
+clusts <- read.csv(here("processed-data", "06_cell_type_clustering", sprintf("banksy_clustering_lambda%s_res%s.csv", lambda, res)))
+clusts <- as.data.frame(clusts)
+print(head(clusts))
+colData(spe)[["Banksy"]] <- as.character(clusts$V1)
+
+
+# Aggregate to broader groups
+
+clusts <- clusts %>% as.data.frame() %>% 
+  mutate(V1 = as.numeric(V1)) %>%
+  mutate(annots=case_when(V1 %in% c(6, 8, 1, 10) ~ "Oligo", # 10 and 15 i am not super sure about
+                          V1 %in% c(7) ~ "Mic",
+                          V1 %in% c(12, 9, 15) ~ "In",
+                          V1 %in% c(18, 11, 14, 2) ~ "Ex",
+                          V1 %in% c(17, 16, 4) ~ "Ast",
+                          V1 %in% c(5, 13, 3) ~ "Endo",
+                       TRUE ~ "NA")) %>%
+    mutate(annots_combined = paste(annots, V1, sep="."))
+  
+colData(spe)[["annots"]] <- clusts$annots
+colData(spe)[["annots_combined"]] <- clusts$annots
+
+
+# Add some slide and date related metadata
+slide_id <- unlist(lapply(strsplit(spe$Sample, split="/"), "[", 9))
+slide_id <- unlist(lapply(strsplit(slide_id, split="__Br"), "[", 1))
+spe$slide_id <- slide_id
+
+run_date <- unlist(lapply(strsplit(spe$Sample, split="/"), "[", 7))
+spe$run_date <- run_date
+
+
+## First, create pseudobulk data:
+
+spe_pseudo <- registration_pseudobulk(
+      spe,
+      var_registration = "annots",
+      var_sample_id = "BrNum",
+      covars = c("Dx", "Age", "Sex", "slide_id", "run_date"),
+      min_ncells = 10,
+      pseudobulk_rds_file = here(
+        "processed-data", "07_cell_type_de",
+        paste0(sprintf("spe_pseudo_donor_celltype_%s", cnm), "", ".rds")
+      )
+    )
+
+# spe_pseudo_donor <- registration_pseudobulk(
+#       spe,
+#       var_registration = "Dx",
+#       var_sample_id = "BrNum",
+#       covars = c("Age", "Sex", "slide_id", "run_date"),
+#       min_ncells = 10,
+#       pseudobulk_rds_file = here(
+#         "processed-data", "05_differential_expression",
+#         paste0("spe_pseudo_donor_", "spaTransfer_k50_smoothed_predictions", ".rds")
+#       )
+#     )
---
+++ code/analysis/07_cell_type_de/02_cell_type_pseudobulk_de.R
@@ -0,0 +1,110 @@
+suppressPackageStartupMessages({
+  library(here)
+  library(SpatialExperiment)
+  library(spatialLIBD)
+  library(scater)
+  library(tidyverse)
+  library(ggrepel)
+  library(sessioninfo)
+})
+
+####################################################################################################
+# Perform DE analysis on the pseudobulked object
+# Reference: https://github.com/LieberInstitute/spatialDLPFC_SCZ/blob/main/code/analysis/pseudobulk_dx/PB_analysis_dx_gene.R
+####################################################################################################
+
+# Load the pseudobulked SPE
+spe_pseudo <- readRDS(here("processed-data", "07_cell_type_de", "spe_pseudo_donor_celltype_clust_M0_lam0.1_k50_res0.7.rds"))
+spe_pseudo$Dx <- factor(spe_pseudo$Dx)
+spe_pseudo$predictions_smooth <- factor(spe_pseudo$annots)
+spe_pseudo$Age <- as.numeric(spe_pseudo$Age)
+
+#### Banksy parameters ####
+lambda <- 0.1
+res <- 0.7 # higher = more clusters
+use_agf <- FALSE
+cnm <- sprintf("clust_M%s_lam%s_k50_res%s", as.numeric(use_agf), lambda, res)
+
+# Create the DE model
+dx_mod <-
+    registration_model(
+      spe_pseudo,
+      covars = c("annots", "Age", "Sex", "slide_id"),
+      var_registration = "Dx"
+    )
+
+
+  dx_block_cor <-
+    registration_block_cor(
+      spe_pseudo,
+      registration_model = dx_mod,
+      var_sample_id = "BrNum"
+    )
+
+  dx_res <- registration_stats_enrichment(
+    spe_pseudo,
+    block_cor = dx_block_cor,
+    covars = c("annots", "Age", "Sex", "slide_id"),
+    var_registration = "Dx",
+    gene_ensembl = "ID",
+    gene_name = "Symbol"
+  )
+
+# Save results
+write.csv(dx_res, here("processed-data", "07_cell_type_de", sprintf("donor_cell_type_level_pseudobulk_Dx_DEGs_.csv", cnm)))
+
+
+pdf(here("plots", "07_cell_type_de", sprintf("donor_cell_type_level_pseudobulk_Dx_DEGs_%s.pdf", cnm)))
+## Volcano Plot ----
+  out_gene_df <- dx_res |>
+    arrange(fdr_SCZ) |>
+    slice_head(n = 1)
+
+  impl_gene_df <- dx_res |>
+    filter(gene %in% c(
+      "PVALB",
+      "NOS1",
+      "SST",
+      "CHODL",
+      "GRIN2A",
+      "SV2A",
+      "DLG4",
+      "C4A",
+      "C3"
+    )) |>
+    select(ensembl, gene, ends_with("SCZ"))
+    
+    sig_gene_df <- dx_res |>
+        filter(fdr_SCZ <= 0.1) |>
+        select(ensembl, gene, ends_with("SCZ"))
+
+  n_sig_gene <- dx_res |>
+    filter(fdr_SCZ <= 0.1) |>
+    nrow()
+
+  print(
+    ggplot(
+      dx_res,
+      aes(
+        x = logFC_SCZ, y = -log10(fdr_SCZ),
+        color = fdr_SCZ <= 0.05
+      )
+    ) +
+      geom_point(alpha = 0.8) +
+      geom_label_repel(
+        data = sig_gene_df,
+        aes(label = gene),
+        force = 2,
+        nudge_y = 0.1
+      ) +
+      labs(
+        title = paste0(
+          "Donor-cell type level pseudobulk analysis by Dx - ", 
+          " ( ", n_sig_gene, " sig genes)"
+        )
+      ) +
+      theme_minimal()
+  )
+dev.off()
+
+
