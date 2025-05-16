7409ea32d2eb4dca8d462ea4379819b014a5a57e
cindyfang70 <cindyfang70@gmail.com>
2025-05-16 14:37:11 -0400

    change proportions heatmap to be ordered by cell type rather than Dx

--- code/analysis/08_cell_type_density/04_cell_type_proportions_in_spds_heatmap.R
+++ code/analysis/08_cell_type_density/04_cell_type_proportions_in_spds_heatmap.R
@@ -7,6 +7,7 @@ suppressPackageStartupMessages({
   library(ggrepel)
   library(sessioninfo)
   library(ComplexHeatmap)
+  library(circlize)
 })
 
 ####################################################################################################
@@ -29,7 +30,7 @@ for (i in 1:length(diags)){
 
 
     # number of cell types in each spatial domain
-    tab <- table(spe_use$spatransfer_k50_predictions_smooth, spe_use$cell_types)
+    tab <- table(spe_use$domain_annotations, spe_use$cell_types)
     tab <- tab/rowSums(tab) # proportions in each spatial domain, sums to 1 across each row
     tab <- as.data.frame(tab)
     tab$Dx <- unique(spe_use$Dx)
@@ -46,8 +47,10 @@ gc()
 
 
 ntc_tab <- all_tabs[[1]]
-cell_types_unique<- unique(ntc_tab$cell_type)
 colnames(ntc_tab)  <- c("spatial_domain", "cell_type", "proportion", "Dx")
+
+cell_types_unique<- unique(ntc_tab$cell_type)
+
 ntc_tab <- ntc_tab %>%
     pivot_wider(names_from = cell_type, values_from = proportion)%>%
     rename_at(vars(all_of(cell_types_unique)),function(x) paste0(x,"_NTC"))
@@ -62,14 +65,14 @@ scz_tab <- scz_tab %>%
 
 all_tab <- cbind(ntc_tab, scz_tab)
 
-# # plot a heatmap of the proportions
-pdf(here("plots", "07_cell_type_de", "cell_type_props_in_spds_heatmap.pdf"), height=10, width=15)
-# Reorder BrNum so NTC bars are on the left
+# plot a heatmap of the proportions
 
 all_tab_plot <- all_tab %>%
   select(-c(spatial_domain, Dx)) %>%
   as.matrix()
 rownames(all_tab_plot) <- all_tab$spatial_domain
+
+all_tab_plot <- all_tab_plot[, order(colnames(all_tab_plot))] # sort the columns by name
 
 column_condition <- ifelse(grepl("NTC$", colnames(all_tab_plot)), "NTC", "SCZ")
 col_ha <- HeatmapAnnotation(
@@ -77,16 +80,29 @@ col_ha <- HeatmapAnnotation(
   col = list(Dx = c("NTC" = "skyblue", "SCZ" = "tomato"))
 )
 
-ordered_cols <- c(
-  colnames(all_tab_plot)[column_condition == "NTC"],
-  colnames(all_tab_plot)[column_condition == "SCZ"]
-)
-all_tab_plot <- all_tab_plot[, ordered_cols]
+# set colour limits
+col_fun <- colorRamp2(c(0, 0.5, 1), c("blue","white", "red"))
+
+pdf(here("plots", "07_cell_type_de", "cell_type_props_in_spds_heatmap.pdf"), height=10, width=15)
+
 
 Heatmap(all_tab_plot, 
           bottom_annotation = col_ha,
-          cluster_columns = FALSE,)
+          cluster_columns = FALSE,
+          cluster_rows=FALSE,
+          col = col_fun,
+          name="Proportion")
 dev.off()
+
+
+
+
+
+
+
+
+
+
 
 
 
