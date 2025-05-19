cb23414f8678f834133930ca5cd47a16d87f66a4
cindyfang70 <cindyfang70@gmail.com>
2025-05-19 11:09:51 -0400

    fix heatmaps for ambiguous clusters

--- code/analysis/07_cell_type_de/06_explore_ambig_clusters.R
+++ code/analysis/07_cell_type_de/06_explore_ambig_clusters.R
@@ -6,28 +6,10 @@ suppressPackageStartupMessages({
   library(tidyverse)
   library(ggrepel)
   library(sessioninfo)
+  library(ComplexHeatmap)
+  library(grid)
 })
 
-
-spe <- readRDS(here("processed-data", "07_cell_type_de", 
-            "cleaned_spe_N24_with_cell_type_and_spds.RDS"))
-
-spe$cell_types <- colData(spe)[["Banksy-clust_M0_lam0.1_k50_res0.7-cell-types"]]
-
-brnums <- unique(spe$BrNum)
-
-spe <- scuttle::logNormCounts(spe)
-set.seed(1034)
-spe <- scater::runPCA(spe, ncomponents=50)
-
-spe <- runUMAP(spe, dimred="PCA")
-
-pdf(here("plots", "07_cell_type_de", "umap_cell_types.pdf"))
-plotReducedDim(spe, dimred="UMAP", colour_by="cell_types")
-plotReducedDim(spe, dimred="UMAP", colour_by="cell_area")
-plotReducedDim(spe, dimred="UMAP", colour_by="BrNum")
-plotReducedDim(spe, dimred="UMAP", colour_by="negProbe_percent")
-dev.off()
 
 # Plot a heatmap of the neg ctrl features for each pseudobulk sample
 spe <- readRDS(here("processed-data", "01_build_spe", "raw_spe_N24.RDS"))
@@ -84,17 +66,17 @@ spe$run_date <- run_date
 
 ## First, create pseudobulk data:
 
-spe_pseudo <- registration_pseudobulk(
-      spe,
-      var_registration = "annots",
-      var_sample_id = "BrNum",
-      covars = c("Dx", "Age", "Sex", "slide_id", "run_date"),
-      min_ncells = 10,
-      pseudobulk_rds_file = here(
-        "processed-data", "07_cell_type_de",
-        paste0(sprintf("spe_pseudo_donor_celltype_with_negctrls_%s", cnm), "", ".rds")
-      )
-    ) # need to rerun 01_create_cell_type_pseudobulk
+# spe_pseudo <- registration_pseudobulk(
+#       spe,
+#       var_registration = "annots",
+#       var_sample_id = "BrNum",
+#       covars = c("Dx", "Age", "Sex", "slide_id", "run_date"),
+#       min_ncells = 10,
+#       pseudobulk_rds_file = here(
+#         "processed-data", "07_cell_type_de",
+#         paste0(sprintf("spe_pseudo_donor_celltype_with_negctrls_%s", cnm), "", ".rds")
+#       )
+#     )
 
 spe_pseudo <- aggregateAcrossCells(spe, ids=DataFrame(cbind(spe$annots, spe$BrNum)))
 spe_pseudo <- logNormCounts(spe_pseudo, size.factors=NULL)
@@ -107,21 +89,75 @@ colnames(plot_counts) <- colnames(spe_pseudo)
 
 dend = cluster_between_groups(plot_counts, colnames(plot_counts))
 
-# Create a column annotation using cell type information
-cell_types <- spe_pseudo$annots
-names(cell_types) <- colnames(spe_pseudo)
+plot_counts <- plot_counts[,order.dendrogram(dend)]
 
-# Create the annotation
+# Set colours of cell types
+# Define custom colors
+celltype_colors <- c(
+  "Ast" = "#A6CEE3",
+  "Endo" = "#1F78B4",
+  "Mic" = "#B2DF8A",
+  "Oligo" = "#33A02C",
+  "L2/3 Ex" = "#FB9A99",
+  "L4/5 Ex" = "#E31A1C",
+  "L5 Ex" = "#FDBF6F",
+  "L6 Ex" = "#FF7F00",
+  "In: SST, PVALB" = "#CAB2D6",
+  "In: VIP, LAMP5" = "#6A3D9A",
+  "Ambig/In/Endo" = "#B15928",
+  "Ambig/Oligo" = "#FF69B4"
+)
+
+# Create a column annotation using cell type information
+cell_types <- colnames(plot_counts)
+# Create the colour annotation
 column_ha <- HeatmapAnnotation(
   CellType = cell_types,
-  annotation_name_gp = gpar(fontsize = 10)
+  annotation_name_gp = gpar(fontsize = 10),
+  col = list(CellType = celltype_colors)
+)
+# Plot the raw counts too
+plot_counts_raw <- as.matrix(counts(spe_pseudo)[grepl("NegControl",rownames(spe_pseudo)),])
+plot_counts_raw <- t(scale(t(plot_counts_raw)))
+colnames(plot_counts_raw) <- colnames(spe_pseudo)
+
+dend_raw = cluster_between_groups(plot_counts_raw, colnames(plot_counts_raw))
+
+plot_counts_raw <- plot_counts[,order.dendrogram(dend_raw)]
+
+# Create a column annotation using cell type information
+cell_types_raw <- colnames(plot_counts_raw)
+#names(cell_types) <- colnames(spe_pseudo)
+
+# Create the annotation
+column_ha_raw <- HeatmapAnnotation(
+  CellType = cell_types_raw,
+  annotation_name_gp = gpar(fontsize = 10),
+  col = list(CellType = celltype_colors)
 )
 
 pdf(here("plots", "07_cell_type_de", "heatmap_ambig_clusters.pdf"))
 
 ComplexHeatmap::Heatmap(plot_counts, cluster_columns=dend,
-        name="logcounts", bottom_annotation = column_ha,
-         row_names_gp = gpar(fontsize = 10))
+        name="logcounts",
+         row_names_gp = gpar(fontsize = 10),
+         column_names_gp=gpar(fontsize = 10), column_names_rot=45,
+         bottom_annotation=column_ha)
+
+ComplexHeatmap::Heatmap(plot_counts_raw, cluster_columns=dend_raw,
+        name="counts", bottom_annotation = column_ha_raw, 
+        row_names_gp = gpar(fontsize = 10), 
+        show_column_names=TRUE)
 
 dev.off()
+
+
+
+
+
+
+
+
+
+
 
