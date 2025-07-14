76dedbb214d860cec07d7c8ec879c17bc87dcc68
cindyfang70 <cindyfang70@gmail.com>
2025-07-14 14:39:39 -0400

    clean up plotting code for the visium logFC vs. xenium logFC scatterplot

--- code/analysis/05_differential_expression/04_compare_visium_xenium_de_logFC.R
+++ code/analysis/05_differential_expression/04_compare_visium_xenium_de_logFC.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 library(here)
 library(ggplot2)
+library(ggrepel)
 
 
 vis_de <- read.csv(here("/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100", 
-            "processed-data/PB_dx_genes",
-            "test_PRECAST_07.csv"))
+            "processed-data/rds/10_dx_deg_adjust_spd/dx-deg_PRECAST07.csv"))
 
 xen_de <- read.csv(here("processed-data", "05_differential_expression", 
     "donor_domain_level_pseudobulk_Dx_DEGs_spaTransfer_k50_smoothed_predictions.csv"))
@@ -27,36 +27,57 @@ write.csv(probe_list_final, here("raw-data",
                 "experiment_info", 
                 "combined_Xenium_SCZ_ProbeSelection5_SHK_v4_15_w_ensemblgene_list.xlsx"))
 
-xen_de_sig <- xen_de %>%
+#xen_de_sig <- xen_de %>%
   filter(fdr_SCZ <= 0.1)
 
-probe_list[probe_list$gene %in% xen_de_sig$gene,]
+#probe_list[probe_list$gene %in% xen_de_sig$gene,]
 
 vis_de <- vis_de %>% 
     mutate(logFC_SCZ = logFC_scz) %>%
-    mutate(p_value_SCZ = p_value_scz) %>% 
-    select(gene, logFC_SCZ, p_value_SCZ) %>%
+    mutate(fdr_SCZ = fdr_scz) %>% 
+    select(gene, logFC_SCZ, fdr_SCZ) %>%
     filter(gene %in% xen_de$gene)
 
 xen_de <- xen_de %>%
-    select(gene, logFC_SCZ, p_value_SCZ) %>%
+    select(gene, logFC_SCZ, fdr_SCZ) %>%
     filter(gene %in% vis_de$gene)
 
 all_de <- merge(vis_de, xen_de, by="gene", all=TRUE, suffixes=c("_vis", "_xen"))
+all_de <- all_de %>%
+  mutate(
+    direction = case_when(
+      logFC_SCZ_vis > 0 & logFC_SCZ_xen > 0 ~ "both upregulated",
+      logFC_SCZ_vis < 0 & logFC_SCZ_xen  < 0 ~ "both downregulated",
+      TRUE ~ "mixed"
+    )
+  )
+
+# create a dataframe of the genes that are significant in xenium
+xen_de_sig <- all_de %>%
+    filter(fdr_SCZ_xen <= 0.1)
+print(xen_de_sig)
 
 pdf(here("plots", "05_differential_expression", "visium_xenium_de_logFC.pdf"))
-ggplot(all_de, aes(x=logFC_SCZ_vis, y=logFC_SCZ_xen)) +
+ggplot(all_de, aes(x=logFC_SCZ_vis, y=logFC_SCZ_xen, colour=direction)) +
     geom_point() +
     geom_abline(intercept=0, slope=1, color="red") +
-    labs(x="Visium logFC", y="Xenium logFC")
+    scale_color_manual(values = c("both upregulated" = "red", "both downregulated" = "blue", "mixed" = "grey"))+
+    labs(x="Visium logFC", y="Xenium logFC")+
+    geom_label_repel(size=3,
+        data = xen_de_sig,
+        aes(label = gene),
+        force = 4,
+        nudge_y = 0.2
+      )+
+      theme_minimal() 
 dev.off()
 
 # Some sanity checks
 sum(!(is.na(probe_list_final$dx_deg)))
 probe_list_final[!is.na(probe_list_final$dx_deg),]
 
-xen_de %>% 
-    filter(fdr_SCZ <= 0.05) %>%
-    nrow()
+# xen_de %>% 
+#     filter(fdr_SCZ <= 0.05) %>%
+#     nrow()
 
 
