080e0227c3598bf8165a0927f142f1e98f1616a7
cindyfang70 <cindyfang70@gmail.com>
2025-07-14 14:40:12 -0400

    code for L2/3 exploratory stuff

--- code/analysis/05_differential_expression/06_layer_stratified_de.R
+++ code/analysis/05_differential_expression/06_layer_stratified_de.R
@@ -15,7 +15,7 @@ suppressPackageStartupMessages({
 # Load the pseudobulked SPE
 spe_pseudo <- readRDS(here("processed-data", "05_differential_expression", "spe_pseudo_donor_domain_spaTransfer_k50_smoothed_predictions.rds"))
 spe_pseudo$Dx <- factor(spe_pseudo$Dx)
-spe_pseudo$predictions_smooth <- factor(spe_pseudo$predictions_smooth)
+spe_pseudo$domain_annotations <- factor(spe_pseudo$domain_annotations)
 spe_pseudo$Age <- as.numeric(spe_pseudo$Age)
 
 
@@ -27,20 +27,28 @@ pdf(here("plots", "05_differential_expression",
         sprintf("stratified_donor_layer_level_PCA_%s.pdf", cnm)),
         height=15, width=20)
 scater::plotPCA(spe_pseudo, colour_by = "Dx", ncomponents=5)
-scater::plotPCA(spe_pseudo, colour_by = "predictions_smooth", shape_by="Dx", ncomponents=5)
+scater::plotPCA(spe_pseudo, colour_by = "domain_annotations", shape_by="Dx", ncomponents=5)
 scater::plotPCA(spe_pseudo, colour_by = "BrNum", ncomponents=5)
 dev.off()
 
 pdf(here("plots", "05_differential_expression", 
         sprintf("stratified_donor_layer_level_pseudobulk_Dx_DEGs_%s.pdf", cnm)))
-# Loop over the cell types and perform DE within each cell type
 
-layers <- unique(spe_pseudo$predictions_smooth)
+
+# Read in Sangho's annotations
+l6_markers <- c("NR4A2", "CABP1", "CRYM", "FILIP1", "CYP46A1")
+l6_markers <- cbind(gene=l6_markers, marker="L6 Ex")
+l6_markers <- as.data.frame(l6_markers)
+
+
+# Loop over the cell types and perform DE within each cell type
+layers <- unique(spe_pseudo$domain_annotations)
 dx_results <- list()
 for(i in 1:length(layers)){
     layer_use <- layers[[i]]
+    
 
-    spe_pseudo_sub <- spe_pseudo[,spe_pseudo$predictions_smooth==layer_use]
+    spe_pseudo_sub <- spe_pseudo[,spe_pseudo$domain_annotations==layer_use]
 
     # Create the DE model
     dx_mod <-
@@ -72,19 +80,6 @@ for(i in 1:length(layers)){
 
    
     ## Volcano Plot ----
-    impl_gene_df <- dx_res |>
-        filter(gene %in% c(
-        "PVALB",
-        "NOS1",
-        "SST",
-        "CHODL",
-        "GRIN2A",
-        "SV2A",
-        "DLG4",
-        "C4A",
-        "C3"
-        )) |>
-        select(ensembl, gene, ends_with("SCZ"))
         
     sig_gene_df <- dx_res |>
         arrange(fdr_SCZ) |>
@@ -95,7 +90,41 @@ for(i in 1:length(layers)){
     n_sig_gene <- dx_res |>
         filter(fdr_SCZ <= 0.1) |>
         nrow()
+    if(layer_use=="L2/3"){
+        dx_res <- merge(dx_res, l6_markers, by="gene", all.x=TRUE)%>%
+            mutate(marker=case_when(
+                marker=="L6 Ex" ~ "L6 Ex",
+                TRUE ~ "Other"
+            ))
+        sig_gene_df <- sig_gene_df %>%
+            mutate(marker=case_when(gene %in% l6_markers$gene ~ "L6 Ex",
+                                    TRUE ~ "Other"))
 
+        sig_gene_df <- rbind(sig_gene_df, dx_res[dx_res$marker=="L6 Ex",])
+        
+        
+        print(
+            ggplot(dx_res,
+            aes(x = logFC_SCZ, y = -log10(fdr_SCZ),
+                color = marker)) +
+            scale_color_manual(values=c("Other"="grey", "L6 Ex" = "blue"))+
+            geom_point(alpha = 0.8) +
+            geom_label_repel(
+                data = sig_gene_df,
+                aes(label = gene),
+                force = 2,
+                nudge_y = 0.1
+            ) +
+            labs(
+                title = paste0(layer_use, 
+                " Donor-layer level pseudobulk analysis by Dx - ", 
+                " ( ", n_sig_gene, " sig genes)"
+                )
+            ) +
+            theme_minimal() 
+    )
+
+    }
     print(
         ggplot(
         dx_res,
