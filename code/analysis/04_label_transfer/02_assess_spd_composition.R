9ab8ade7f3635b6a1390318f8732e55040ef5301
cindyfang70 <cindyfang70@gmail.com>
2025-09-22 21:25:45 -0400

    fix PCA plotting

--- code/analysis/04_label_transfer/02_assess_spd_composition.R
+++ code/analysis/04_label_transfer/02_assess_spd_composition.R
@@ -8,7 +8,7 @@ library(SpatialExperiment)
 # see what's driving the variation in the pseudobulked samples
 ####################################################################################################
 
-res <- readRDS(here("processed-data", "04_label_transfer", "label_transfer_N24_k50.rds"))
+#res <- readRDS(here("processed-data", "04_label_transfer", "label_transfer_N24_k50.rds"))
 
 # pdf(here("plots", "04_label_transfer", "label_transfer_faceted_plots.pdf"), width = 10, height = 10)
 # for (i in 1:length(res$targets)){
@@ -38,10 +38,10 @@ res <- readRDS(here("processed-data", "04_label_transfer", "label_transfer_N24_k
 #   gc()
 # }
 # dev.off()
-spe_all <- do.call(cbind, res$targets)
-rm(res)
-gc()
-print(spe_all)
+# spe_all <- do.call(cbind, res$targets)
+# rm(res)
+# gc()
+# print(spe_all)
 
 # labs <- as.data.frame(colData(spe_all)[,"predictions_smooth"])
 # rownames(labs) <- colnames(spe_all)
@@ -50,7 +50,32 @@ print(spe_all)
 #   here("processed-data", "04_label_transfer", "label_transfer_N24_k50_smoothed_labels.csv"))
 
 
-spe_all$sample_id <- paste(spe_all$BrNum, spe_all$Dx, sep="_")
+# spe_all$sample_id <- paste(spe_all$BrNum, spe_all$Dx, sep="_")
+# domain_annotations <- colData(spe_all) %>%
+#   as.data.frame() %>%
+#   mutate(domain_annotations = case_when(predictions_smooth == "spd07" ~ "L1",
+#                                         predictions_smooth == "spd06" ~ "L2/3",
+#                                         predictions_smooth == "spd02" ~ "L3/4",
+#                                         predictions_smooth == "spd05" ~ "L5",
+#                                         predictions_smooth == "spd03" ~ "L6",
+#                                         predictions_smooth == "spd01" ~ "WMtz",
+#                                         predictions_smooth == "spd04" ~ "WM",
+#                                           TRUE ~ NA))
+
+
+spe_all <- readRDS(here("processed-data", "01_build_spe", "raw_spe_N24.RDS"))
+outlier_ids <- read.csv(here("processed-data", "02_xenium_qc", "outlier_ids.csv"))$x
+spe_all <- spe_all[, -which(colnames(spe_all) %in% outlier_ids)]
+rm(outlier_ids)
+spds <- read.csv(here("processed-data", "04_label_transfer", "label_transfer_N24_k50_smoothed_labels.csv"))
+
+rownames(spds) <- spds$X 
+spds$X <- NULL
+colnames(spds) <- c("predictions_smooth")
+stopifnot(all(colnames(spe_all)==rownames(spds)))
+colData(spe_all)$predictions_smooth <- factor(spds$predictions_smooth)
+
+# map spd style labels to actual annotations
 domain_annotations <- colData(spe_all) %>%
   as.data.frame() %>%
   mutate(domain_annotations = case_when(predictions_smooth == "spd07" ~ "L1",
@@ -62,8 +87,8 @@ domain_annotations <- colData(spe_all) %>%
                                         predictions_smooth == "spd04" ~ "WM",
                                           TRUE ~ NA))
 
-
-
+colData(spe_all)$domain_annotations <- domain_annotations$domain_annotations
+print(head(spe_all$domain_annotations))
 colData(spe_all)$domain_annotations <- domain_annotations$domain_annotations
 
 spe_pseudo <- scuttle::aggregateAcrossCells(spe_all, 
