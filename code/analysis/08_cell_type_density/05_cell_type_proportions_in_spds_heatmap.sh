a8c774a2dd770f1e071363652b761f48fc2f6171
cindyfang70 <cindyfang70@gmail.com>
2025-09-24 20:21:50 -0400

    slurm submission script

---
+++ code/analysis/08_cell_type_density/05_cell_type_proportions_in_spds_heatmap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=05_cell_type_proportions_in_spds_heatmap
+#SBATCH --mem=55G
+#SBATCH --time=02:00:00
+#SBATCH -n 1
+#SBATCH --output=logs/%x.txt
+#SBATCH --error=logs/%x.txt    # file to collect standard output
+
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_CLUSTER_NAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load Modules
+module load conda_R/4.3.x
+
+## List current modules for reproducibility
+module list
+
+## Run code
+Rscript 05_cell_type_proportions_in_spds_heatmap.R
+
+
+## Memeory stat
+#sstat -a -o JobID,MaxVMSizeNode,MaxVMSize,AveVMSize,MaxRSS,AveRS S,MaxDiskRead,MaxDiskWrite,AveCPUFreq,TRESUsageInMax -j ${SLURM_JOB_ID}
+
+echo "**** Job ends ****"
+date
