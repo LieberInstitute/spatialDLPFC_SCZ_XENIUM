f84e2578418d5d075672f0381b6dbd9a34d96cdc
cindyfang70 <cindyfang70@gmail.com>
2025-10-23 10:23:14 -0400

    initialize script for cellchat analysis

---
+++ code/analysis/09_visium_LR_analysis/CellChat/01_run_cellchat.R
@@ -0,0 +1,10 @@
+library(scater)
+library(CellChat)
+library(Seurat)
+library(dplyr)
+
+
+#####################################################################
+# Run CellChat across each of the samples, using
+# spots from all microenvironments.
+#####################################################################
