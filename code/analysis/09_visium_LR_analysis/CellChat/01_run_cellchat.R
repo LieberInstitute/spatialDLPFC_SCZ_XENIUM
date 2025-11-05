80d59aebf84c545f2cb4fa4de598deae3e0972f4
cindyfang70 <cindyfang70@gmail.com>
2025-11-05 14:19:50 -0500

    cellchat code

--- code/analysis/09_visium_LR_analysis/CellChat/01_run_cellchat.R
+++ code/analysis/09_visium_LR_analysis/CellChat/01_run_cellchat.R
@@ -2,9 +2,96 @@ library(scater)
 library(CellChat)
 library(Seurat)
 library(dplyr)
+library(here)
 
 
 #####################################################################
 # Run CellChat across each of the samples, using
 # spots from all microenvironments.
 #####################################################################
+
+
+source(here("code", "analysis", "09_visium_LR_analysis", "CellChat", "modeling.R"))
+args <- commandArgs(trailingOnly = TRUE)
+i <- as.numeric(args[[1]])
+
+
+spe <- readRDS(
+  here::here(
+    "/dcs04/lieber/marmaypag/spatialDLPFC_SCZ_LIBD4100/",
+    "processed-data/rds/01_build_spe",
+    "fnl_spe_kept_spots_only.rds"
+  )
+)
+
+# Check if any spots belong to multiple microenvironments
+table(spe$microenv)
+table(rowSums(as.data.frame(colData(spe))[,c("neuropil_pos", "neun_pos", "pnn_pos", "vasc_pos")]))
+#      0      1      2      3      4 
+#  74195 175675  28283   1632     21 
+
+
+brnums <- unique(spe$BrNumbr)
+
+br_use <- brnums[[i]]
+spe_use <- spe[,spe$BrNumbr == br_use]
+print(sprintf("Running CellChat on sample %s", br_use))
+
+data.input <- logcounts(spe_use)
+rownames(data.input) <- rowData(spe_use)$gene_name # Convert emsembl id to gene sym
+
+
+# loop over samples and run cellchat for each sample
+microenvs <- c("neuropil", "neun", "pnn", "vasc")
+for (j in 1:length(microenvs)){
+
+    me_use <- microenvs[[j]]
+    print(sprintf("  Microenvironment: %s", me_use))
+
+    # create object for cellchat
+    colData(spe_use)$is_microenv <- colData(spe_use)[,grepl(me_use, colnames(colData(spe)))]
+
+    meta <- as.data.frame(SingleCellExperiment::colData(spe_use))
+    meta$samples <- meta$sample_id |> factor()
+    cellchat <- createCellChat(
+      object = data.input, meta = meta,
+      group.by = "is_microenv")
+
+    unique(cellchat@idents)
+
+    # add metadata to cellchat object
+    cellchat <- addMeta(cellchat, meta = meta)
+    cellchat <- setIdent(cellchat, ident.use = "is_microenv") 
+    groupSize <- as.numeric(table(cellchat@idents))
+
+    # Attach database
+    cellchat@DB <- CellChatDB.human
+
+    cellchat <- subsetData(cellchat)
+    cellchat <- identifyOverExpressedGenes(cellchat, do.DE = FALSE)
+    cellchat <- identifyOverExpressedInteractions(cellchat)
+    cellchat <- computeCommunProb(cellchat)
+    cellchat <- filterCommunication(cellchat, min.cells = 5)
+    
+    comm_df <- subsetCommunication(cellchat)
+
+    print(head(comm_df))
+
+
+    write.csv(comm_df,
+      file = here::here(
+        "results",
+        "09_visium_LR_analysis",
+        "CellChat",
+        sprintf("cellchat_communications_BrNumbr_%s_microenv_%s.csv",
+          br_use,
+          me_use)
+      ),
+      row.names = FALSE
+    )
+
+
+}
+
+    
+
---
+++ code/analysis/09_visium_LR_analysis/CellChat/01_run_cellchat.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=01_run_cellchat
+#SBATCH --mem=100G
+#SBATCH --time=20:30:00
+#SBATCH -n 1
+#SBATCH --output=logs/%x_%a.out
+#SBATCH --error=logs/%x_%a.out   # file to collect standard output
+#SBATCH --array=1-24
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${SLURM_CLUSTER_NAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## Load Modules
+module load conda_R/4.3.x
+
+## List current modules for reproducibility
+module list
+
+## Run code
+Rscript 01_run_cellchat.R ${SLURM_ARRAY_TASK_ID}
+
+
+## Memeory stat
+#sstat -a -o JobID,MaxVMSizeNode,MaxVMSize,AveVMSize,MaxRSS,AveRS S,MaxDiskRead,MaxDiskWrite,AveCPUFreq,TRESUsageInMax -j ${SLURM_JOB_ID}
+
+echo "**** Job ends ****"
+date
